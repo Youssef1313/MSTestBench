4d291af95ad89fcc6a15873ac83a551034786d47
Youssef1313 <youssefvictor00@gmail.com>
2025-08-12 07:43:51 +0200

    Switch to BDN

---
+++ MSTestBench/MTPEntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text;
+
+namespace MSTestBench;
+
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public sealed class MicrosoftTestingPlatformEntryPoint
+{
+    public static async global::System.Threading.Tasks.Task<int> Main(string[] args)
+    {
+        global::Microsoft.Testing.Platform.Builder.ITestApplicationBuilder builder = await global::Microsoft.Testing.Platform.Builder.TestApplication.CreateBuilderAsync(args);
+        // SelfRegisteredExtensions.AddSelfRegisteredExtensions(builder, args);
+        builder.AddMSTest(() => [typeof(MicrosoftTestingPlatformEntryPoint).Assembly]);
+        using (global::Microsoft.Testing.Platform.Builder.ITestApplication app = await builder.BuildAsync())
+        {
+            return await app.RunAsync();
+        }
+    }
+}
---
+++ MSTestBench/Program.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+BenchmarkRunner.Run<Bench>();
+
+[Config(typeof(Config))]
+[MemoryDiagnoser]
+public class Bench
+{
+    private class Config : ManualConfig
+    {
+        public Config()
+        {
+            var job = Job.Default
+                .WithStrategy(RunStrategy.Monitoring) // only monitoring
+                .WithWarmupCount(3) // light warm-up
+                .WithIterationCount(10) // only 10 interations
+                .WithLaunchCount(1); // launch a test only once
+            var jobs = new List<Job>();
+            foreach (var version in (ReadOnlySpan<string>)["3.8.3", "3.9.3", "3.10.1", "3.11.0-preview.25408.2"])
+            {
+                foreach (var testScenario in (ReadOnlySpan<string>)["SingleClass10KTests", "ThousandClass200Tests", "HundredClass2000Tests"])
+                {
+                    jobs.Add(job.WithNuGet(new NuGetReferenceList(
+                    [
+                        new NuGetReference("MSTest.TestFramework", version),
+                        new NuGetReference("MSTest.TestAdapter", version)
+                    ])).WithCustomBuildConfiguration("Debug").WithArguments([new MsBuildArgument($"/p:TestScenario={testScenario}")]));
+                }
+            }
+
+            AddJob(jobs.ToArray());
+
+            // add the percentage column
+            SummaryStyle =
+                BenchmarkDotNet.Reports.SummaryStyle.Default
+                .WithRatioStyle(BenchmarkDotNet.Columns.RatioStyle.Percentage);
+            Options |= ConfigOptions.DisableOptimizationsValidator | ConfigOptions.GenerateMSBuildBinLog;
+        }
+    }
+
+    [Benchmark]
+    public async Task Benchmark()
+    {
+        await MSTestBench.MicrosoftTestingPlatformEntryPoint.Main([]);
+    }
+}
